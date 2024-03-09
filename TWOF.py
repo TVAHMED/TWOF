bb696b029f2a791788e6c3d40f9767d13ec11829
TVAHMED <160422099+TVAHMED@users.noreply.github.com>
2024-03-09 16:51:42 +0500

    Update TWOF.py
    pkg update
    pkg upgrade 
    pkg install python
    pkg install git
    git clone https://github.com/TVAHMED/TWOF 
    cd TWOF
    ls
    python TWOF.py

--- TWOF.py
+++ TWOF.py
@@ -182,11 +182,7 @@ def checks(ok,cp):
 loop = 0
 cp = []
 ok = []
-twf = []
-
-
-
-    os.system("clear")
+twf =
     print(logo)
     print("\t  Facebook : MUHAMMAD HAMID KHAWAJA")
     print("\t  Fb page  : Hamii Hbf Cammands")
